c10988f4f32a7177a4735441b899d827f5d352da
Edgar Cruz <edgarcruztrejo0@gmail.com>
2022-11-22 21:32:13 -0600

    Correcciones

--- Backend/script/consultas.php
+++ Backend/script/consultas.php
@@ -6,7 +6,7 @@
             require $route.'Backend/conexion.php';
             $today = date('Y-m-d');
             $days_ago = date("Y-m-d", strtotime($today."- 30 days"));
-            $query = "SELECT * FROM prestamos_rentas WHERE fecha BETWEEN '$days_ago' AND '$today';";
+            $query = "SELECT * FROM prestamos_rentas WHERE fecha >'$days_ago';";
             $result = $conexion->query($query);
             return $result;
         }
@@ -17,7 +17,7 @@
                 return $name;
             }
             else{
-                header("location:../../index.php");
+                header("location:../index.php");
             }
         }
     }
